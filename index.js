ab15e50c13231c00a528262170de6726a1416994
rashik513 <mdrobiulislamrashikpdf@gmail.com>
2025-05-21 16:59:14 +0600

    add delete api method

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
@@ -30,9 +30,23 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/groups/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await hobbyCollection.findOne(query);
+      res.send(result);
+    });
+
     app.post("/groups", async (req, res) => {
       const newGroup = req.body;
       const result = await hobbyCollection.insertOne(newGroup);
+      res.send(result);
+    });
+
+    app.delete("/groups/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await hobbyCollection.deleteOne(query);
       res.send(result);
     });
 
