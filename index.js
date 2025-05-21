f0524f5fc289f5a424bdefe41aea084e642a28d8
rashik513 <mdrobiulislamrashikpdf@gmail.com>
2025-05-21 18:53:18 +0600

    add put api method

--- index.js
+++ index.js
@@ -43,6 +43,22 @@ async function run() {
       res.send(result);
     });
 
+    app.put("/groups/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const group = req.body;
+      const updatedDoc = {
+        $set: group,
+      };
+      const result = await hobbyCollection.updateOne(
+        filter,
+        updatedDoc,
+        options
+      );
+      res.send(result);
+    });
+
     app.delete("/groups/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
